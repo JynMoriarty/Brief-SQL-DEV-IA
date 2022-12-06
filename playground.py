4b04cede1d4fe60faa6b6b10f40567c7524bd9f4
Machtelinck <subaruparia@gmail.com>
2022-12-06 22:36:56 +0100

    petit_ajout

---
+++ playground.py
@@ -0,0 +1,22 @@
+from faker import Faker
+from faker.providers import BaseProvider
+import random
+f = Faker("fr_FR")
+
+# ajouter une seed 
+
+print(f.name())
+print(f.address())
+# print(f.zipcode()) pas utilisable pour la France X'( 
+
+for _ in range(10):
+    print(f.unique.random_int(min=1,max=10))
+
+class tuned_provider(BaseProvider):
+
+    def metier_rest(self):
+        return random.choice(["Cuisinier","Serveur","Directeur","Manager"])
+
+
+f.add_provider(tuned_provider)
+print(f.metier_rest())
